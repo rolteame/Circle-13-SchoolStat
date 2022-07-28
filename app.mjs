3650c30e1c92ed30af0d4fcdd3602471ffb4a5e7
Rotimi Adebiyi <r.s.adebiyi@gmail.com>
2022-07-28 14:05:29 +0000

    temporary navigation and javascript

--- app.mjs
+++ app.mjs
@@ -1,15 +1,18 @@
+import {instructors, students} from "./data.mjs";
 function startApp() {
     // Your entire app should not necessarily be coded inside this 
     // single function (though there's no penalty for that), 
     // so create and use/call additional functions from here
   
     // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
+  let numberOfInstructors = document.getElementById('instructorsNumber');
+  let instructorNumbers = instructors.length;
+  numberOfInstructors.innerText = instructorNumbers;
   
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
-    }
+  let numberOfStudents = document.getElementById('studentsNumber');
+  let studentNumbers = students.length;
+  numberOfStudents.innerText = studentNumbers;
+  
   };
   
   // ======= DO NOT EDIT ============== //
---
+++ data.mjs
@@ -0,0 +1,64 @@
+export let instructors = [
+  {
+  name: "Instructor 1",
+  track: "Frontend",
+  course: "Introduction to HTML",
+  },
+  {
+  name: "Instructor 2",
+  track: "Backend",
+  course: "Introduction to Javascript",
+  },
+  {
+  name: "Instructor 1",
+  track: "Cloud",
+  course: "What is cloud computing",
+  },
+]
+
+export let students = [
+  {
+    name: "Student 1",
+    track: "Frontend",
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Student 2",
+    track: "Backend",
+    gender: "Female",
+    age: 20,
+  },
+  {
+    name: "Student 3",
+    track: "Frontend",
+    gender: "Female",
+    age: 22,
+  },
+  {
+    name: "Student 4",
+    track: "cloud",
+    gender: "male",
+    age: 28,
+  },
+  {
+    name: "Student 5",
+    track: "Backend",
+    gender: "male",
+    age: 23,
+  },
+  {
+    name: "Student 6",
+    track: "cloud",
+    gender: "male",
+    age: 25,
+  },
+  {
+    name: "Student 1",
+    track: "Frontend",
+    gender: "male",
+    age: 24,
+  },
+]
+
+
